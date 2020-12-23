e5745d61e7caf43b1a74c76d34aebf1afa2a7019
Andrew Toups <mistertoups@gmail.com>
2020-12-23 12:36:13 +0000

    Initial commit for poem component.

---
+++ scripts/components/poem/poem.js
@@ -0,0 +1,41 @@
+define(['ko'], function(ko){
+    return function(params){
+        let self = this;
+        console.log(params.birthChart);
+
+        self.showPlacements = ko.observable(false);
+        let bdate = params.birthData.date;
+        let blocation = params.birthData.location;
+        let bcoords = params.birthData.coord;
+        self.birthDay = ko.observable(`${bdate.getMonth()+1}/${bdate.getDate()}/${bdate.getFullYear()} -- ${bdate.getHours()+1}:${(bdate.getMinutes()<10?'0':'')+bdate.getMinutes()} UTC`);
+        self.coords = ko.observable(`${bcoords[0]},${bcoords[1]}`);
+        self.location = ko.observable(`${blocation.city}, ${blocation.state} ${blocation.country}`);
+
+        self.sun = ko.observable(params.birthChart.sun);
+        self.mars = ko.observable(params.birthChart.mars);
+        self.rising = ko.observable(params.birthChart.ascendant);
+        self.venus = ko.observable(params.birthChart.venus);
+        self.moon = ko.observable(params.birthChart.moon);
+        self.mercury = ko.observable(params.birthChart.mercury);
+
+        self.poemData = ko.observableArray();
+        self.isLoading = ko.observable(true)
+        self.isLoading.subscribe(newValue => {
+            vm.isLoading(newValue);
+        });
+        fetch('https://api.2psy.net/orbData/poem')
+        .then(response => response.json())
+        .then(data => self.poemData(data));
+        self.lines = ko.computed(() => {
+            return self.poemData().map(line => {
+                let planet = Object.keys(line)[0];
+                return {
+                    line: line[planet][self[planet]()],
+                    placement: `${self[planet]()} ${planet}`
+                };
+            });
+        });
+        document.querySelector('main').focus();
+        self.isLoading(false);
+    };
+});
