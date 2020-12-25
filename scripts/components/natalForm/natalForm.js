4d082c6dbea62e1e33f7a8e2a99a15730000622b
Andrew Toups <mistertoups@gmail.com>
2020-12-25 22:48:11 +0000

    Refactors loading spinner to use svg background-image. Adds various svgs for loading spinner.

--- scripts/components/natalForm/natalForm.js
+++ scripts/components/natalForm/natalForm.js
@@ -233,6 +233,7 @@ define([
             if (!newValue) self.coordinates(null);
         });
         
+        self.suggestionsLoading = ko.observable(false);
         suggestionFields.forEach(type => {
             self[type+'Results'] = ko.observableArray([]);
             self[type+'Selected'] = ko.observable();
@@ -245,13 +246,17 @@ define([
             self[type+'Query'] = ko.computed(() => self[type+'Input']());
             self[type+'Query'].subscribe(newValue => { self.geoLookupQuery(newValue, self[type+'Results'], type) });
             self[type+'Query'].extend({rateLimit: 50});
+            
+            self[type+"resultsLoading"] = ko.observable(false);
+            self[type+"resultsLoading"].subscribe(newValue => self.suggestionsLoading(newValue));
     
             self[type+'Params'] = {
                 query: self[type+'Input'],
                 results: self[type+'Results'],
                 selected: self[type+'Selected'],
                 value: self.currentLocationValue,
-                type: type
+                type: type,
+                status: self[type+"resultsLoading"]
             };
         });
 
@@ -262,6 +267,7 @@ define([
             if (!self.suggestionFieldsReady()) return true;
             if (self.loadingTZ()) return true;
             if (self.loadingGeoResults()) return true;
+            if (self.suggestionsLoading()) return true;
             if (self.loadingRandom()) return true;
             return false;
         });
