89733adab869c9418e44a32c4e662c9e2d105906
Andrew Toups <mistertoups@gmail.com>
2020-12-27 08:35:56 +0000

    Extends clean module to apply to textInput binding. Adds new binding to suggestion component. Rewrites styles to apply consistently between suggestion inputs and selection inputs.

--- scripts/utils/clean.js
+++ scripts/utils/clean.js
@@ -1,49 +1,58 @@
 define(['ko'], function(ko) {
+    let cleanInit = (element, valueAccessor, allBindings, viewModel, bindingContext) => {
+        let v = valueAccessor();
+
+        v.startingValue = typeof v.startingValue === 'undefined' ? v() : v.startingValue;
+        v.clean = ko.observable(true);
+        v.untouched = ko.observable(true);
+        v.touched = ko.computed(() => !v.untouched());
+        v.dirty = ko.computed(() => !v.clean());
+
+        bindingContext.$data.cleanReady = ko.observable(false);
+
+        let cleanSub = v.subscribe(newValue => {
+            newValue = /^\d+$/.test(newValue) ? parseInt(newValue) : newValue;
+            if (newValue !== v.startingValue) {
+                v.clean(false);
+                cleanSub.dispose();
+            }
+        });
+
+        let touch = event => {
+            v.untouched(false);
+            element.removeEventListener('blur', touch);
+        };
+        element.addEventListener('blur', touch);
+
+        let cssRules = {
+            untouched: v.untouched,
+            touched: v.touched,
+            clean: v.clean,
+            dirty: v.dirty
+        };
+        ko.applyBindingsToNode(element, {css: cssRules}, bindingContext.$data);
+
+        ko.bindingEvent.subscribe(element, 'descendantsComplete', function () {
+            bindingContext.$data.cleanReady(true);
+        });
+ 
+        // startPossiblyAsyncContentBinding is necessary for descendant bindings to notify us of their completion
+        var innerBindingContext = ko.bindingEvent.startPossiblyAsyncContentBinding(element, bindingContext);
+    }
     return (function() {
         ko.bindingHandlers.cleanValue = {
             preprocess: function(value, name, addBinding){
                 addBinding("value", value);
                 return value;
             },
-            init: (element, valueAccessor, allBindings, viewModel, bindingContext) => {
-                let v = valueAccessor();
-
-                v.startingValue = typeof v.startingValue === 'undefined' ? v() : v.startingValue;
-                v.clean = ko.observable(true);
-                v.untouched = ko.observable(true);
-                v.touched = ko.computed(() => !v.untouched());
-                v.dirty = ko.computed(() => !v.clean());
-
-                bindingContext.$data.cleanReady = ko.observable(false);
-
-                let cleanSub = v.subscribe(newValue => {
-                    if (newValue !== v.startingValue) {
-                        v.clean(false);
-                        cleanSub.dispose();
-                    }
-                });
-
-                let touch = event => {
-                    v.untouched(false);
-                    element.removeEventListener('blur', touch);
-                };
-                element.addEventListener('blur', touch);
-
-                let cssRules = {
-                    untouched: v.untouched,
-                    touched: v.touched,
-                    clean: v.clean,
-                    dirty: v.dirty
-                };
-                ko.applyBindingsToNode(element, {css: cssRules}, bindingContext.$data);
-
-                ko.bindingEvent.subscribe(element, 'descendantsComplete', function () {
-                    bindingContext.$data.cleanReady(true);
-                });
-         
-                // startPossiblyAsyncContentBinding is necessary for descendant bindings to notify us of their completion
-                var innerBindingContext = ko.bindingEvent.startPossiblyAsyncContentBinding(element, bindingContext);
-            }
+            init: cleanInit
+        }
+        ko.bindingHandlers.cleanTextInput = {
+            preprocess: function(value, name, addBinding){
+                addBinding("textInput", value);
+                return value;
+            },
+            init: cleanInit
         }
     })();
 });
