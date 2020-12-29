3ad068e41442bc6c3e0651e6b5628b1c33a3b840
Andrew Toups <mistertoups@gmail.com>
2020-12-29 06:11:36 +0000

    Refactors options placeholder binding to play nice on mobile.

--- scripts/utils/optionsPlaceholder.js
+++ scripts/utils/optionsPlaceholder.js
@@ -1,7 +1,14 @@
 define(['ko'], function(ko){
     return (function(){
-        let hide = element => { element.classList.add('hide'); };
-        let show = element => { element.classList.remove('hide'); }
+        let hide = element => {
+            element.classList.add('hide');
+            element.disabled = true;
+        };
+        let show = element => {
+            element.classList.remove('hide');
+            element.disabled = false;
+        };
+        let forceChange = element => { element.dispatchEvent(new Event('change')); }
 
         ko.bindingHandlers.optionsPlaceholder = {
             preprocess: function(value, name, addBinding) {
@@ -11,25 +18,33 @@ define(['ko'], function(ko){
             init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
                 let placeholderText = ko.isObservable(valueAccessor()) ? String(valueAccessor()()) : String(valueAccessor());
                 let listeners = [
-                    'mousedown', 'blur', 'change'
+                    'mousedown', 'focusout', 'touchstart', 'blur', 'change'
                 ];
+                valueAccessor.touchEvent = false;
 
                 let autoSelect = function(e){
                     let options = Array.from(element.querySelectorAll('option'));
-                    let placeholderEl = options.find(o => o.innerText === placeholderText);
-
-                    if (e.type === 'mousedown') {
+                    let placeholderEl = options.find(o => o.innerText === placeholderText && !o.value);
+                    let defaultEl = options.find(o => o.innerText === placeholderText && o.value) ?
+                        options.find(o => o.innerText === placeholderText && o.value) : false;
+                    if (defaultEl && !element.value) element.value = defaultEl.value;
+                    if (e.type === 'touchstart') valueAccessor.touchEvent = true;
+                    
+                    if (e.type === 'mousedown' || e.type === 'touchstart') {
                         if (e.target === element) hide(placeholderEl);
-                        else element.value = e.target.value;
+                        else {
+                            element.value = e.target.value;
+                            forceChange(element);
+                        }
                     }
                     else if (e.type === 'blur') show(placeholderEl);
+                    else if (e.type === 'focusout' && valueAccessor.touchEvent) {
+                        element.value = defaultEl ? defaultEl.value : options[0].value;
+                        forceChange(element);
+                    }
                     else if (e.type === 'change') {
                         element.removeChild(placeholderEl);
-                        if (!element.value) {
-                            options = Array.from(element.querySelectorAll('option'));
-                            let defaultEl = options.find(o => o.innerText === placeholderText) ? options.find(o => o.innerText === placeholderText) : false;
-                            element.value = defaultEl ? defaultEl.value : options[0].value;
-                        }
+                        element.value = defaultEl ? defaultEl.value : options[0].value;
                         listeners.forEach(event => { element.removeEventListener(event, autoSelect)});
                     }
                 };
