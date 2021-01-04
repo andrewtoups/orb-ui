df74aa4bc5ef3db70b5b8427d3f34f2c70879c65
Andrew Toups <mistertoups@gmail.com>
2021-01-04 23:22:02 +0000

    Removes mobile message calls used in testing.

--- scripts/utils/optionsPlaceholder.js
+++ scripts/utils/optionsPlaceholder.js
@@ -54,8 +54,6 @@ define(['ko'], function(ko){
 
                     if (browsing) {
                         hide(v.placeholderEl);
-                        mm(`placeholder "${v.placeholderEl.innerText}"'s class is: ${v.placeholderEl.classList[0]}`);
-                        mm(`placeholder "${v.placeholderEl.innerText}"'s disabled status is: ${v.placeholderEl.disabled}`);
                     }
 
                     if (defaultElIsntFirst) element.value = !element.value && v.defaultEl ? v.defaultEl.value : element.value;
