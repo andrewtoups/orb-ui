b1d0ebcb1d50305f739a867545a31987e8e87010
Andrew Toups <mistertoups@gmail.com>
2021-01-27 18:36:42 +0000

    Adds aliases to requirejs config.

--- scripts/rConfig.js
+++ scripts/rConfig.js
@@ -3,7 +3,11 @@ requirejs.config({
     paths: {
         'ko': 'vendor/knockout',
         'text': 'vendor/text',
-        'api': 'utils/api'
+        'api': 'utils/api',
+        'clean': 'utils/clean',
+        'inputMode': 'utils/inputModeSwitcher',
+        'optionsPlaceholder': 'utils/optionsPlaceholder',
+        'transitionState': 'utils/transitionState'
     }
 });
 
