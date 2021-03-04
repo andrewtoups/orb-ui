dadbcb0155b1d3cbbd03752312a1cba0e024c0d6
Andrew Toups <mistertoups@gmail.com>
2021-03-04 00:36:12 +0000

    Adds icon files and accompanying stylesheets and data.

---
+++ scripts/dataStore/icons.js
@@ -0,0 +1,24 @@
+define([], function(){
+    return [
+        'alarm-clock',
+        'ball-gag',
+        'book',
+        'cassette',
+        'compact-mirror',
+        'dynamite',
+        'flask',
+        'game-controller',
+        'heart-lock',
+        'knife',
+        'lantern',
+        'letter',
+        'lipstick',
+        'magic-wand',
+        'microphone',
+        'mushroom',
+        'pie',
+        'plant',
+        'rope',
+        'wishbone'
+    ];
+});
